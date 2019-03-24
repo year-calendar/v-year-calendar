9d1ce8a98d17e23286ad29dae4450c9dc502b797
Paul-DS <paul.david.sivelle@gmail.com>
2019-03-24 18:34:02 +0100

    Implement a basic wrapper

---
+++ rollup.config.js
@@ -0,0 +1,67 @@
+import nodeResolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import vue from "rollup-plugin-vue";
+import postcss from 'rollup-plugin-postcss';
+import commonjs from "rollup-plugin-commonjs";
+import { uglify } from "rollup-plugin-uglify";
+import pkg from "./package.json";
+
+var defaultConfig = {
+  input: './src/v-year-calendar.vue',
+  plugins: [
+    vue({
+      css: true
+    }),
+    postcss(),
+    nodeResolve({
+      jsnext: true,
+      extensions: [".js", ".vue"]
+    }),
+    babel({
+      exclude: "node_modules/**",
+      extensions: ['.js', '.vue'],
+    }),
+    commonjs()
+  ],
+  external: Object.keys(pkg.peerDependencies)
+};
+
+export default [
+  // CJS
+  {
+    ...defaultConfig,
+    output: {
+      file: "./dist/v-year-calendar.js",
+      format: "cjs"
+    },
+  },
+  {
+    input: './dist/v-year-calendar.js',
+    output: {
+      file: './dist/v-year-calendar.min.js',
+      format: "cjs"
+    },
+    plugins: [uglify()]
+  },
+
+  // UMD
+  {
+    ...defaultConfig,
+    output: {
+      file: "./dist/v-year-calendar.umd.js",
+      format: "umd",
+      name: "Calendar",
+      globals: {
+        vue: "Vue"
+      }
+    },
+  },
+  {
+    input: './dist/v-year-calendar.umd.js',
+    output: {
+      file: './dist/v-year-calendar.umd.min.js',
+      format: "umd"
+    },
+    plugins: [uglify()]
+  }
+];
---
+++ src/v-year-calendar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import JsCalendar from 'js-year-calendar';
+import 'js-year-calendar/dist/js-year-calendar.css';
+
+export default {
+  name: 'Calendar',
+  mounted: function() {
+      this.calendar = new JsCalendar(this.$el);
+  }
+}
+</script>
